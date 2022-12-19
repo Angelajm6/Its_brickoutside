2621b4d4165d75d92898f34343442a19c65d04b7
angelajm6 <angelajaume.m@gmail.com>
2022-12-19 16:39:48 -0500

    for loops to get min and max temperatures

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,17 @@ function GetInfo(){
     cityName.innerHTML = "--" + newname.value + "--"
 }
 
+//link for the weather API
+fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=6ab94a05776e0c97a5014f2b05bcf440") 
+.then(response => response.jsoon())
+//get min temperature
+.then(date =>{
+for(i=0; i<5; i++) {
+    document.getElementById("day" + (i+1) + "Min").innerHTML = "Min:" + number(data.list[i].main.temp_min -274.66).toFixed(1)+"&#176"
+}
+//get max temperature
+for(i=0; i<5; i++) {
+    document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + number(data.list[i].main.temp_max -274.84).toFixed(1)+"&#176"
+}
+})
+
