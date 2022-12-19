1d19dca6d5bab9bce6fd3071fd14dc55f9fe29d5
angelajm6 <angelajaume.m@gmail.com>
2022-12-19 17:40:08 -0500

    Changes in styling and html modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,59 @@
-function GetInfo(){
-    var newname = document.getElementById("city-input");
-    var cityName = document.getElementById("city-name");
-    cityName.innerHTML = "--" + newName.value + "--";
+function GetInfo() {
 
-//link for the weather API
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=6ab94a05776e0c97a5014f2b05bcf440") 
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=32ba0bfed592484379e51106cef3f204')
 .then(response => response.json())
-
-//get min temperature
 .then(data => {
-for(i=0; i<5; i++) {
-    document.getElementById("day" + (i+1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -274.66).toFixed(1)+ "°";
-}
-//get max temperature
-for(i=0; i<5; i++) {
-    document.getElementById("day" + (i+1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max -274.84).toFixed(1)+ "°";
-}
 
-//get icons for every image
-for(i = 0; i<5; i++){
-    document.getElementById("img" +(i+1)).src = "http://openweathermap.org/img/wn/" + 
-    data.list[i].weather[0].icon + ".png";
+    //Getting the min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+        //Number(1.3450001).toFixed(2); // 1.35
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+    //------------------------------------------------------------
+
+    //Getting Weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    //------------------------------------------------------------
+    console.log(data)
+
+
 })
 
-//In case there's an error, display an error message
-.catch(err => alert("something went wrong!"))
+.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
 }
 
-
-//Display New York weather by default
 function DefaultScreen(){
-    document.getElementById("city-input").defaultValue = "New York";
+    document.getElementById("cityInput").defaultValue = "London";
     GetInfo();
 }
 
-//Display the different Days
+
+//Getting and displaying the text for the upcoming five days of the week
 var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
 //Function to get the correct integer for the index of the days array
 function CheckDay(day){
-    if(day +d.getDay() > 6){
-        return day + d.getDay() -7;
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
     }
-    else {
+    else{
         return day + d.getDay();
     }
 }
 
-for (i = 0; i<5;i++){
-    document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
-}
-
-
-
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
